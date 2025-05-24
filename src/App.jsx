734fda6f2eb30313ce89cccbc2be1f89604fe88f
dannyarena <arenadanny71@gmail.com>
2025-05-24 17:25:45 +0200

    add active class to change btn color based on language selected

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,10 @@ function App() {
       {/* funzione che crea un bottone per ogni linguaggio */}
       {languages.map((language) => (      
         <button 
-        key={language.id} onClick={() => setSelectedLanguage(language)}>
+        key={language.id}
+        onClick={() => setSelectedLanguage(language)}
+        // classe active che permette di cambiare colore al btn
+        className={selectedLanguage?.id === language.id ? 'active' : ''}>
           {language.title}
         </button>
       ))}
